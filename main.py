70f4883e31ae6daa191c5d61eff584e9c96bf4d5
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2022-06-14 10:48:56 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 
-print('Hello')
+userNum = int(input())
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end = '\n') # Output formatting issue here; fix it when instructed
