b188d00625252ac6c89ca3bf0a8cff3e3788914d
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2022-06-21 19:00:42 -0400

    Update lab622.py

--- homework2/lab622.py
+++ homework2/lab622.py
@@ -1,1 +1,18 @@
+#Dumkele Osegi, PSID 1894081
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = int(input())
+e = int(input())
+f = int(input())
+
+res = False
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        if a * x + b * y == c and d * x + e * y == f:
+            print(x,y)
+            res = True
+if not res:
+    print('No solution')
 
