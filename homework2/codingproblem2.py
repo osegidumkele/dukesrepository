cc2cbd0b9714529e4bf8888ddbdc3677529789b6
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2022-06-22 03:54:30 -0400

    Update codingproblem2.py

--- homework2/codingproblem2.py
+++ homework2/codingproblem2.py
@@ -6,16 +6,20 @@ import datetime
 def find(timeformat):
     months_num = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7,
                   "August": 8,   "September": 9, "October": 10, "November": 11, "December": 12}
-    year = timeformat.split(",")[-1].strip()
-    month = timeformat.split(",")[0]
-    day = timeformat.split(",")[0].split()[-1]
-    newmonth = months_num[month]
-    return str(newmonth)+"/"+day+"/"+year
+    try:
+        year = timeformat.split(",")[-1].strip()
+        month = timeformat.split(",")[0]
+        day = timeformat.split(",")[0].split()[-1]
+        newmonth = months_num[month]
+        return str(newmonth)+"/"+day+"/"+year
+    except:
+        return ""
 
 
-while True:
-    userinput = input()
-    if userinput == "-1":
-        break
-    print(find(userinput))
+with open("inputDates.txt") as f:
+    for y in f.readlines():
+        if y.strip() != "-1":
+            print(find(y.strip()))
+            
+
 
