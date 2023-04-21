25f598aef9496984beb6d56c9ac69422e9b99e76
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2023-04-21 16:51:27 -0500

    Create sqltable.sql

---
+++ Project_2/sqltable.sql
@@ -0,0 +1,6 @@
+Create table snowboard (id int(6) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                    boardtpe varchar(255),
+                    brand varchar(255),
+                    msrp varchar(255),
+                    size varchar(255)
+                    )
