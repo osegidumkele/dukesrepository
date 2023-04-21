b0a1602cd0c4c92415812b93a53aaf8bea0c8a30
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2023-04-21 16:31:56 -0500

    Delete homework1 directory

--- homework1/lab119.py
+++
@@ -1,5 +0,0 @@
-
-userNum = int(input())
-userNumSquared = userNum * userNum # Bug here; fix it when instructed
-
-print(userNumSquared, end = '\n') # Output formatting issue here; fix it when instructed
