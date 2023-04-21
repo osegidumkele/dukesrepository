a1eaabdd6311537562fdc85fabedf1fa5fb1b739
osegidumkele <107004638+osegidumkele@users.noreply.github.com>
2023-04-21 16:36:13 -0500

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-#Dumkele Osegi, PSID 1894081
-userNum = int(input())
-userNumSquared = userNum * userNum # Bug here; fix it when instructed
-
-print(userNumSquared, end = '\n') # Output formatting issue here; fix it when instructed
